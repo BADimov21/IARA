a29fe380c69d746f921b66b3c2a9dba08ff84ec0
Bozhidar Dimov <BADimov21@codingburgas.bg>
2025-12-01 23:16:26 +0200

    feat: create base service and base service injector

---
+++ API/IARA/IARA.Infrastructure/Base/BaseService.cs
@@ -0,0 +1,13 @@
+﻿using IARA.Persistence.Data;
+
+namespace IARA.Infrastructure.Base;
+
+public class BaseService
+{
+    public BaseService(BaseServiceInjector injector)
+    {
+        Db = injector.Context;
+    }
+    
+    public IARADbContext Db { get; set; }
+}
---
+++ API/IARA/IARA.Infrastructure/Base/BaseServiceInjector.cs
@@ -0,0 +1,13 @@
+﻿using IARA.Persistence.Data;
+
+namespace IARA.Infrastructure.Base;
+
+public class BaseServiceInjector
+{
+    public BaseServiceInjector(IARADbContext DbContext)
+    {
+        Context = DbContext;
+    }
+
+    public IARADbContext Context { get; set; }
+}
